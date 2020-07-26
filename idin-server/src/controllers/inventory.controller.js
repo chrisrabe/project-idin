f13af396a2e4f78d5e5899bc73db61bb54df734d
Chris Rabe <chris.rabe14@gmail.com>
2020-07-26 20:33:39 +1000

    added skeleton inventory

---
+++ idin-server/src/controllers/inventory.controller.js
@@ -0,0 +1,20 @@
+const database = require('../_util/database');
+const errorType = require('../_util/constants/error.types');
+const AppError = require('../_util/api.error');
+
+exports.getInventoryList = async (orgId) => {
+
+};
+
+exports.createInventory = async () => {
+
+};
+
+exports.updateInventory = async () => {
+
+};
+
+exports.deleteInventory = async () => {
+
+};
+
--- idin-server/src/routes/v1/inventory.route.js
+++ idin-server/src/routes/v1/inventory.route.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 
-router.get('/', asyncHandler(async(req, res) => {
+// get all inventory associated with the organisation
+router.get('/:orgId', asyncHandler(async(req, res) => {
 	return res.noContent();
 }));
+
+// add new inventory
+router.post('/', asyncHandler(async (req, res) => {
+	return res.noContent();
+}));
+
+// update inventory
+router.post('/:id', asyncHandler(async (req, res) => {
+
+}));
+
+// remove inventory
+router.delete('/:id', asyncHandler(async (req, res) => {
+
+}));
+
 
 module.exports = router;
