1bf75a4a4d5f4310f929b33fb310aa6dd4024234
Chris Rabe <chris.rabe14@gmail.com>
2020-07-28 21:03:20 +1000

    created skeleton for landing

--- idin-client/src/components/routes/Landing/index.jsx
+++ idin-client/src/components/routes/Landing/index.jsx
@@ -1,5 +1,94 @@
 import React from 'react';
+import bgImage from 'assets/img/bg-image.png';
+import cfcImage from 'assets/img/cfc-logo.svg';
+import idinLogo from 'assets/img/idin-logo.png';
+import styled from 'styled-components';
+import {Grid, Typography} from '@material-ui/core';
+import SignInForm from 'components/routes/Landing/SignInForm';
+import {
+  StyledImage,
+} from './styles';
 
-const Landing = () => <div>Landing</div>;
+const MainContainer = styled.div`
+  display: flex;
+`;
+
+const BodyContainer = styled(Grid)`
+  position: absolute;
+  height: 100%;
+  padding: 20px;
+`;
+
+const FormContainer = styled(Grid)`
+  padding-top: 5%;
+  padding-right: 10%;
+`;
+
+const LogoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const IDINLogo = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+
+const ParagraphContainer = styled.div`
+  background-color: #333333;
+  border-radius: 20px;
+  padding: 20px;
+  width: 500px;
+`;
+
+const BigText = styled(Typography)`
+  color: white;
+  font-size: 40px;
+  text-align: center;
+`;
+
+const AppTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+`;
+
+const AppTitleText = styled(Typography)`
+  font-size: 60px;
+  color: white;
+  font-weight: 500;
+`;
+
+const AppDetailsContainer = styled(Grid)`
+  padding-left: 5%;
+`;
+
+const Landing = () => (
+  <MainContainer>
+    <StyledImage src={bgImage} />
+    <BodyContainer container spacing={1}>
+      <Grid container item xs={12} direction="row">
+        <AppDetailsContainer item container xs={6} alignItems="center">
+          <LogoContainer>
+            <IDINLogo src={idinLogo} />
+            <AppTitleContainer>
+              <AppTitleText variant="body1">PROJECT IDIN</AppTitleText>
+            </AppTitleContainer>
+          </LogoContainer>
+          <ParagraphContainer>
+            <BigText>Stay on top of your inventory through our predictive analysis</BigText>
+          </ParagraphContainer>
+        </AppDetailsContainer>
+        <FormContainer item container xs={6} alignContent="center" justify="flex-end">
+          <SignInForm />
+        </FormContainer>
+      </Grid>
+      <Grid container item xs={12} alignItems="flex-end">
+        <img src={cfcImage} alt="Call For Code Logo" />
+      </Grid>
+    </BodyContainer>
+  </MainContainer>
+);
 
 export default Landing;
---
+++ idin-client/src/components/routes/Landing/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const StyledImage = styled.img`
+  width: 100%;
+  z-index: -1;
+`;
