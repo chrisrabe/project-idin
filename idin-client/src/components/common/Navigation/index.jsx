bc3c07977bf18d8023c64a92d1338592edfd7b50
Chris Rabe <chris.rabe14@gmail.com>
2020-07-29 01:21:36 +1000

    added navigation header

--- idin-client/src/components/common/Navigation/index.jsx
+++ idin-client/src/components/common/Navigation/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import NavLogo from './NavLogo';
 
-const Navigation = () => <div>Navigation</div>;
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 300px;
+  background-color: #333333;
+  display: flex;
+`;
+
+const Navigation = () => (
+  <MainContainer>
+    <NavLogo />
+  </MainContainer>
+);
 
 export default Navigation;
