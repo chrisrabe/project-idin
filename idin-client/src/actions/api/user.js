eda00b3c31e6454ced36af90bd84e1c0472a3008
Chris Rabe <chris.rabe14@gmail.com>
2020-07-29 21:12:57 +1000

    added register

--- idin-client/src/actions/api/user.js
+++ idin-client/src/actions/api/user.js
@@ -6,11 +6,12 @@ import {
 } from '../app.actions';
 import {
   createUserRequest,
-  createUserSuccess,
+  signIn,
 } from '../home.actions';
 
 const { server } = config;
 const baseRoute = `${server.baseUrl}${server.api.v1}${server.route.user}`;
+const orgRoute = `${server.baseUrl}${server.api.v1}${server.route.organisation}`;
 
 export const getUserList = () => async (dispatch) => {
   dispatch(requestUsers());
@@ -19,12 +20,34 @@ export const getUserList = () => async (dispatch) => {
   dispatch(receiveUsers(users));
 };
 
-export const createUser = (username, email) => async (dispatch) => {
+export const createUser = (username, email, org) => async (dispatch) => {
   dispatch(createUserRequest());
   const reqData = {
     username,
     email,
   };
-  const { data } = await axios.post(baseRoute, reqData);
-  dispatch(createUserSuccess(data.data.id));
+  const res = await axios.post(baseRoute, reqData);
+  const userId = res.data.data.id;
+  let orgId;
+  if (org.id) {
+    const newData = {
+      email,
+      organisationId: org.id,
+    };
+    // edit user to join organisation
+    await axios.post(`${baseRoute}/${userId}`, newData);
+    orgId = org.id;
+  } else {
+    const newData = {
+      name: org.orgName,
+      isSupplier: org.isSupplier,
+      userId,
+    };
+    // create org
+    const { data } = await axios.post(orgRoute, newData);
+    orgId = data.data.id;
+  }
+  // if org id doesn't exist. create org
+  // if org id exists, update user
+  dispatch(signIn(userId, orgId));
 };
